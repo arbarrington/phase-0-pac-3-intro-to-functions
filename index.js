c902ca4520b1d060b87bf120fe566bfbddc25683
Austin Barrington <arbarrington12@gmail.com>
2022-07-21 18:02:19 -0500

    commit tree dd7d971690f6c6f808bca553b7f6f8d156a10eb8

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Follow along with the examples here
+function percInc(initial, final) {
+    console.log(`Your percentage change was:`);
+    return ((final - initial) / initial);
+}
+
+console.log(percInc(54, 78));
